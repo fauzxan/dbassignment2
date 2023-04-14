8c63a6578f7f0d6bbd77896be2293b7f694a9c5d
Fauzaan <fauzaan.mm@gmail.com>
2023-04-14 13:16:38 +0800

    q1

--- student_files/try.py
+++ student_files/try.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import os
 
 df = pd.read_csv("student_files/data/TA_restaurants_curated_cleaned.csv")
 
 
 df_filtered = df.filter(df.Rating > 1.0)
-
-
+if not os.mkdir("student_files/data/output"):
+    os.mkdir("student_files/data/output")
+df_filtered.to_csv("student_files/data/output/TA_restaurants_curated_cleaned.csv")
 
