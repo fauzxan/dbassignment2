e2dca8fe903c7b5db204b92895ef30789633e127
Fauzaan <fauzaan.mm@gmail.com>
2023-04-14 12:44:54 +0800

    Update q1.py

--- student_files/q1.py
+++ student_files/q1.py
@@ -10,3 +10,8 @@ spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
 df = spark.read.option("header",True).csv(f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/")
 df.printSchema()
+
+df_filtered = df.filter(df.rating > 1)
+
+# Print the filtered rows
+df_filtered.show()
