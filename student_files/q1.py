2ea1290c3913e8420565c7d9feb92a657be1ef93
Fauzaan <fauzaan.mm@gmail.com>
2023-04-14 13:04:08 +0800

    Update q1.py

--- student_files/q1.py
+++ student_files/q1.py
@@ -10,7 +10,7 @@ except Exception as e:
     print(f"Error: {e}")
     print("<Usage>: spark-submit spark://<master>:7077 q1.py <hdfs_namenode>")
     sys.exit(1)
-    
+
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 # YOUR CODE GOES BELOW
 df = spark.read.option("header",True).csv(f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/")
@@ -18,6 +18,6 @@ df.printSchema()
 
 df_filtered = df.filter(df.Rating > 1.0)
 
-df_filtered.write.csv(f"hdfs://{hdfs_nn}:9000/assignment2/part1/output/q1_output.csv")
+df_filtered.write.csv(f"hdfs://{hdfs_nn}:9000/assignment2/part1/output/")
 # Print the filtered rows
 df_filtered.show()
