23f29aff3bcafefcc26293509fb5aecc040c2e5c
Visshal Natarajan <99006087+Dr-Lazarus@users.noreply.github.com>
2023-04-15 10:59:36 +0800

    Update q3.py

--- student_files/q3.py
+++ student_files/q3.py
@@ -16,20 +16,19 @@ df = spark.read.option("header", True).option("inferSchema", True).csv("hdfs://{
 df = df.select(fn.col('ID_TA'), fn.col('Reviews'))
 
 # Converts the "[['Just like home', 'A Warm Welcome to Wintry Amsterdam'], ['01/03/2018', '01/01/2018']]" to a List datatype 
-def tolist(string_list):
-    list = ast.literal_eval(string_list)
-    return list
-
-# Using rdd apis here
-df = df.rdd
-df = df.map(lambda cols: ( cols[0],tolist(cols[1])[0], tolist(cols[1])[1] )) #[0] is ID_TA, The second one would be the list of reviews, the third one would be the list of dates 
+def Maketolist(intput_str):
+    array = ast.literal_eval(input_str)
+    return array
 # Using RDD to utilise map functionality 
+df = df.rdd
+df = df.map(lambda cols: ( cols[0],Maketolist(cols[1])[0], Maketolist(cols[1])[1] )) #[0] is ID_TA, The second one would be the list of reviews, the third one would be the list of dates 
 
 df = df.toDF(["ID_TA", "review", "date"])
 
-df = df.withColumn('tmp', fn.arrays_zip("review", "date")).withColumn('tmp', fn.explode('tmp')).select("ID_TA" , fn.col("tmp.review"), fn.col("tmp.date")) # review and  date columns are represented as lists of strings (zip) following which each review and its corresponding date are represented as separate rows (explode).
+df = df.withColumn('new', fn.arrays_zip("review", "date")).withColumn('new', fn.explode('new')).select("ID_TA" , fn.col("new.review"), fn.col("new.date")) # review and  date columns are represented as lists of strings (zip) following which each review and its corresponding date are represented as separate rows (explode).
 
 # Sanity Check
 df.show()
+
 df.write.csv("hdfs://%s:9000/assignment2/output/question3/" % (hdfs_nn), header=True)
 
