9fd04915301f01cf8dab8cc5d663602a78c27b88
Visshal Natarajan <99006087+Dr-Lazarus@users.noreply.github.com>
2023-04-15 10:35:02 +0800

    Update q3.py

--- student_files/q3.py
+++ student_files/q3.py
@@ -1,5 +1,7 @@
 import sys
 from pyspark.sql import SparkSession
+from pyspark.sql import functions as fn
+import ast
 
 # you may add more import if you need to
 
@@ -9,3 +11,24 @@ hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 3").getOrCreate()
 # YOUR CODE GOES BELOW
+df = spark.read.option("header", True).option("inferSchema", True).csv("hdfs://{}:9000/assignment2/part1/input/".format(hdfs_nn))
+
+df = df.select(fn.col('ID_TA'), fn.col('Reviews'))
+
+# Converts the "[['Just like home', 'A Warm Welcome to Wintry Amsterdam'], ['01/03/2018', '01/01/2018']]" to a List datatype 
+def tolist(string_list):
+    list = ast.literal_eval(string_list)
+    return list
+
+# Using rdd apis here
+df = df.rdd
+df = df.map(lambda cols: ( cols[0],tolist(cols[1])[0], tolist(cols[1])[1] )) #[0] is ID_TA, The second one would be the list of reviews, the third one would be the list of dates 
+
+df = df.toDF(["ID_TA", "review", "date"])
+
+df = df.withColumn('tmp', fn.arrays_zip("review", "date")).withColumn('tmp', fn.explode('tmp')).select("ID_TA" , fn.col("tmp.review"), fn.col("tmp.date"))
+
+# Sanity Check
+df.show()
+df.write.csv("hdfs://%s:9000/assignment2/output/question3/" % (hdfs_nn), header=True)
+
