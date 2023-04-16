0486bc079e134b0c3a86ca8e987f06e26a009da2
Fauzaan <fauzaan.mm@gmail.com>
2023-04-17 01:09:29 +0800

    Update q5.py

--- student_files/q5.py
+++ student_files/q5.py
@@ -59,7 +59,7 @@ final_df = (
 )
 
 # Sanity check
-final_df.show()
+# final_df.show()
 
 final_df.write.option("header", True).parquet(
     "hdfs://%s:9000/assignment2/part2/output/question5/" % (hdfs_nn)
